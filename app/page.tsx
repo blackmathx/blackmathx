2f2120cdbe3869208504564c70f4b7b739a53a41
bo <jepayton12013@gmail.com>
2025-07-11 15:19:04 -0400

    update index

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,112 @@ export default function HomePage() {
 
 	return (
 		<>
-			
+			<div className="w-[70%] m-auto">
+
+
+				<div className="flex items-start p-2">
+					<span className="text-gray-500 text-sm mb-0 pr-2 pt-1 whitespace-nowrap min-w-[90px]">
+						06-11-2025
+					</span>
+					<div className="flex-grow">
+						<a href="github.com/blackmathx" target="_blank">
+							<div className="font-semibold mb-0 text-base">Github</div>
+						</a>
+						<div className="text-gray-600 text-sm mb-2">(github.com)</div>
+						<div>
+							My Github Acount
+						</div>
+					</div>
+				</div >
+
+
+				<div className="flex items-start p-2">
+					<span className="text-gray-500 text-sm mb-0 pr-2 pt-1 whitespace-nowrap min-w-[90px]">
+						06-11-2025
+					</span>
+					<div className="flex-grow">
+						<a href="https://www.linkedin.com/in/dean-payton-7427b1280/" target="_blank">
+							<div className="font-semibold mb-0 text-base">LinkedIn</div>
+						</a>
+						<div className="text-gray-600 text-sm mb-2">(linkedin.com)</div>
+						<div>
+							LinkedIn Profile (blank)
+						</div>
+					</div>
+				</div >
+
+				<div className="flex items-start p-2">
+					<span className="text-gray-500 text-sm mb-0 pr-2 pt-1 whitespace-nowrap min-w-[90px]">
+						06-10-2025
+					</span>
+					<div className="flex-grow">
+						<a href="https://blackmathx.com" target="_blank">
+							<div className="font-semibold mb-0 text-base">blackmathx.com</div>
+						</a>
+						<div className="text-gray-600 text-sm mb-2">(blackmathx.com)</div>
+						<div>
+							Blackmathx.com Website
+						</div>
+					</div>
+				</div >
+
+
+				<div className="flex items-start p-2">
+					<span className="text-gray-500 text-sm mb-0 pr-2 pt-1 whitespace-nowrap min-w-[90px]">
+						06-10-2025
+					</span>
+					<div className="flex-grow">
+						<a href="https://github.com/blackmathx/python-reference" target="_blank">
+							<div className="font-semibold mb-0 text-base">Python Language Reference</div>
+						</a>
+						<div className="text-gray-600 text-sm mb-2">(github.com)</div>
+
+						<div>
+							Personal Python language reference on github.com
+						</div>
+
+					</div>
+				</div >
+				<div className="flex items-start p-2">
+					<span className="text-gray-500 text-sm mb-0 pr-2 pt-1 whitespace-nowrap min-w-[90px]">
+						06-10-2025
+					</span>
+					<div className="flex-grow">
+						<a href="https://github.com/blackmathx/csharp-reference" target="_blank">
+							<div className="font-semibold mb-0 text-base">C# Language Reference</div>
+						</a>
+						<div className="text-gray-600 text-sm mb-2">(github.com)</div>
+
+						<div>
+							Personal C# language reference on github
+						</div>
+
+					</div>
+				</div >
+				<div className="flex items-start p-2">
+					<span className="text-gray-500 text-sm mb-0 pr-2 pt-1 whitespace-nowrap min-w-[90px]">
+						06-10-2025
+					</span>
+					<div className="flex-grow">
+						<a href="https://github.com/blackmathx/java-reference" target="_blank">
+							<div className="font-semibold mb-0 text-base">Java Language Reference</div>
+						</a>
+						<div className="text-gray-600 text-sm mb-2">(github.com)</div>
+
+						<div>
+							Personal Java language reference on github.com
+						</div>
+
+					</div>
+				</div >
+
+
+			</div>
+
+
+
+
+
 
 		</>
 	)
